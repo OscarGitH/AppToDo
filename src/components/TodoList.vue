85a9d8d84c5d52db540757c075aa2749aff93d78
OscarGitH <pavoine.e2202961@etud.univ-ubs.fr>
2024-05-22 14:13:50 +0200

    init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo-list">
+    <div v-for="task in tasks" :key="task.id">
+      {{ task.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tasks']
+}
+</script>
+
+<style scoped>
+/* Styles spécifiques au composant TodoList */
+</style>
---
+++ src/components/TypeList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="TypeList">
+    <!-- Contenu de la première zone -->
+    <h1>Zone supérieure</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // Logique de votre composant TypeListe.vue si nécessaire
+}
+</script>
+
+<style scoped>
+/* Styles spécifiques au composant ZoneSup */
+</style>
